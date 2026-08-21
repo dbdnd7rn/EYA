71c2bde4ccf636e39c13396c6f29f3274dfea87f
dbdnd7rn <144498376+dbdnd7rn@users.noreply.github.com>
2026-08-21 16:47:21 +0200

    ui: link ticket wallet to transfer centre

--- app/(student)/market/my-tickets.tsx
+++ app/(student)/market/my-tickets.tsx
@@ -1,5 +1,12 @@
+import { View } from "react-native";
 import MyTicketsScreen from "@/components/market/MyTicketsScreen";
+import TicketTransferShortcut from "@/components/market/TicketTransferShortcut";
 
 export default function StudentMyTicketsPage() {
-  return <MyTicketsScreen />;
+  return (
+    <View style={{ flex: 1 }}>
+      <MyTicketsScreen />
+      <TicketTransferShortcut />
+    </View>
+  );
 }
